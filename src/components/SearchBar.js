f703051d91ef2cad15b1e802aa4ebc79bd88e56b
joutria <joutria@gmail.com>
2021-07-05 18:50:36 -0500

    changing resolution for unknown city

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,13 @@ function SearchBar(props) {
   async function clickSearch() {
     if (props.value) {
       const data = await fetch(props.url).then((res) => res.json());
-      props.setData(data);
-      document.querySelector('.container').classList.remove('hidden');
-      document.querySelector("#cf").classList.remove("hidden");
+      if (data.error) {
+        alert('Oops, it seems that we don\'t have information about that city')
+      }else{
+        props.setData(data);
+        document.querySelector(".container").classList.remove("hidden");
+        document.querySelector("#cf").classList.remove("hidden");
+      }
     }
   }
 
